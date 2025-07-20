f9762c23f3f0b70a14e651ff60a1c69fd4b26813
doitchuu <seulgichu14@gmail.com>
2025-07-20 16:16:08 +0900

    feat: TodoHeader 컴포넌트 구현

---
+++ components/TodoHeader.js
@@ -0,0 +1,15 @@
+function TodoHeader($container) {
+  this.$container = $container;
+
+  this.render = function () {
+    this.$container.innerHTML = `
+      <img src="./assets/logo.svg" class="image-logo" alt="로고" />
+      <h3 class="header-title">Todolist</h3>
+      <p>간단히 할 일을 추가하고,<br />완료하면서 성취감을 느껴보세요.</p>
+    `;
+  };
+
+  this.render();
+}
+
+export default TodoHeader;
