37d3e117b0cd144ba892ff036031fa803ef5c584
doitchuu <seulgichu14@gmail.com>
2025-07-20 20:10:23 +0900

    fix: model 목데이터 구조 수정

--- model/model.js
+++ model/model.js
@@ -1,9 +1,11 @@
 export const model = [
   {
+    id: "1753009468965",
     name: "리액트 복습",
     isCompleted: true,
   },
   {
+    id: "1753009468933",
     name: "타입스크립트 공부",
     isCompleted: false,
   },
