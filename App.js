7137414e078bc498e59e5acb4cf18cb5c8735b69
doitchuu <seulgichu14@gmail.com>
2025-07-20 20:10:03 +0900

    fix: App 내 컴포넌트 렌더링 로직 추가 및 onAdd 로직 추가

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
+import TodoHeader from "./components/TodoHeader.js";
+import TodoCount from "./components/TodoCount.js";
 import TodoInput from "./components/TodoInput.js";
 import TodoList from "./components/TodoList.js";
+import TodoControls from "./components/TodoControls.js";
+
 import { model } from "./model/model.js";
 
 function App() {
   this.data = [];
-  this.todoList = null;
+  this.todoHeader = null;
+  this.todoCount = null;
   this.todoInput = null;
+  this.todoList = null;
+  this.TodoControls = null;
 
   this.isEditing = false;
   this.editingText = "";
-  this.$todoList = document.querySelector("#todo-list");
+  this.totalCount = 0;
+  this.completedCount = 0;
 
   this.createAppLayout = () => {
     document.getElementById("todo-list").innerHTML = `
@@ -17,13 +25,17 @@ function App() {
       <div class="todo-content">
         <div id="todo-content-title"></div>
         <div id="todo-input"></div>
-        <div id="todo-list"></div>
+        <div id="todo-items"></div>
         <div id="todo-controls"></div>
       </div>
     `;
   };
 
   this.render = () => {
+    this.todoHeader = new TodoHeader(document.getElementById("todo-header"));
+    this.todoCount = new TodoCount(
+      document.getElementById("todo-content-title")
+    );
     this.todoInput = new TodoInput({
       $container: document.getElementById("todo-input"),
       isEditing: this.isEditing,
@@ -31,8 +43,35 @@ function App() {
       onAdd: this.onAdd,
       onUpdate: this.onUpdate,
     });
-    this.todoList = new TodoList(this.$todoList, this.data);
+    this.todoList = new TodoList(
+      document.getElementById("todo-items"),
+      this.data
+    );
+    this.todoControls = new TodoControls(
+      document.getElementById("todo-controls"),
+      this.totalCount,
+      this.completedCount
+    );
   };
+
+  this.onAdd = (text) => {
+    const todo = text.trim();
+
+    if (todo) {
+      const newTodos = [
+        ...this.data,
+        {
+          id: Date.now().toString(),
+          name: todo,
+          isCompleted: false,
+        },
+      ];
+
+      this.setState(newTodos);
+    }
+  };
+
+  this.onUpdate = () => {};
 
   this.init = () => {
     this.data = model;
@@ -40,8 +79,9 @@ function App() {
     this.render();
   };
 
-  this.setState = () => {
-    // TODO: 할 일 추가, 삭제 로직 추가 예정
+  this.setState = (newData) => {
+    this.data = newData;
+    this.render();
   };
 
   this.init();
