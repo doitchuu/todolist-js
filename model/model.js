4f35475faaee0c6b77f04b9296949b91cfcc0683
doitchuu <seulgichu14@gmail.com>
2025-07-21 22:01:28 +0900

    fix: model 구조 변경

--- model/model.js
+++ model/model.js
@@ -1,12 +1,10 @@
-export const model = [
-  {
-    id: "1753009468965",
+export const model = {
+  1753009468965: {
     name: "리액트 복습",
     isCompleted: true,
   },
-  {
-    id: "1753009468933",
+  1753009468933: {
     name: "타입스크립트 공부",
     isCompleted: false,
   },
-];
+};
