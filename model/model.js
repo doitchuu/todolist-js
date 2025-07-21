471f52db23cc426441151a83ace40961dfa66c83
doitchuu <seulgichu14@gmail.com>
2025-07-21 23:06:01 +0900

    fix: model 구조 재수정(createAt 추가)

--- model/model.js
+++ model/model.js
@@ -2,9 +2,11 @@ export const model = {
   1753009468965: {
     name: "리액트 복습",
     isCompleted: true,
+    createAt: "1753009468965",
   },
   1753009468933: {
     name: "타입스크립트 공부",
     isCompleted: false,
+    createAt: "1753009468933",
   },
 };
